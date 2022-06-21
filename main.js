2dfcc19c49ded0d1bb2b1701dc8851d98f6bd41a
Alex <zuedev@gmail.com>
2022-06-21 01:16:31 +0000

    DRY code

--- main.js
+++ main.js
@@ -2,37 +2,46 @@ import "dotenv/config";
 
 switch (process.argv[2]) {
   case "complete":
-    fetch("https://api.openai.com/v1/completions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: "text-davinci-002",
-        prompt: process.argv[3],
-        max_tokens: 4000,
-      }),
-    }).then((response) =>
-      response.json().then((data) => console.log(data.choices[0].text))
-    );
+    complete(process.argv[3]).then(parseResponse);
     break;
   case "edit":
-    fetch("https://api.openai.com/v1/edits", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-      },
-      body: JSON.stringify({
-        model: "text-davinci-edit-001",
-        input: process.argv[3],
-        instruction: process.argv[4],
-      }),
-    }).then((response) =>
-      response.json().then((data) => console.log(data.choices[0].text))
-    );
+    edit(process.argv[3], process.argv[4]).then(parseResponse);
     break;
   default:
     throw new Error("Unknown command.");
+}
+
+const config = {
+  fetchHeaders: {
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${process.env.API_KEY}`,
+  },
+};
+
+function complete(prompt) {
+  return fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: config.fetchHeaders,
+    body: JSON.stringify({
+      model: "text-davinci-002",
+      prompt: prompt,
+      max_tokens: 4000,
+    }),
+  });
+}
+
+function edit(input, instruction) {
+  return fetch("https://api.openai.com/v1/edits", {
+    method: "POST",
+    headers: config.fetchHeaders,
+    body: JSON.stringify({
+      model: "text-davinci-edit-001",
+      input: input,
+      instruction: instruction,
+    }),
+  });
+}
+
+function parseResponse(response) {
+  return response.json().then((data) => console.log(data.choices[0].text));
 }
